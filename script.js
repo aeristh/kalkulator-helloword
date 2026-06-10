8dcaea444399affe4dd43100cac76061ac6968b6
Diva A iFarence <divaanggunil025@gmail.com>
2026-06-10 09:29:37 +0700

    menambahkan script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    // Berapapun inputnya, hasil selalu Hello World
+    display.value = "Hello World";
+}
